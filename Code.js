79a0a436e1289f1c4f2ec2b49a7a717aec38f2f8
鈴木吉輝 <yoshiki@MacBook-Air.local>
2020-07-23 15:13:57 +0900

    bootstrap入れてみた

--- Code.js
+++ Code.js
@@ -1,3 +1,16 @@
-function myFunction() {
-  
+function doGet() {
+  const items = new Items();
+  items.read();
+  return HtmlService.createHtmlOutputFromFile('index');
+}
+
+class Items {
+  read() {
+    const fileId = '1HSCQjyH0pTjz_PmDkY6ymsiS2qGAQW46W43x_Eqrues';
+    const ss = SpreadsheetApp.openById(fileId);
+    const sheetName = 'item_list';
+    const sheet = ss.getSheetByName(sheetName);
+    const values = sheet.getDataRange().getValues();
+    console.log(values);
+  }
 }
